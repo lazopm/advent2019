4f6c49bf1766df2b3b95ec91355760deaf1289a8
Pablo Lazo <lazopm@gmail.com>
2019-12-03 03:40:10 -0800

    ahhhhh

--- 3/part2.py
+++ 3/part2.py
@@ -1,35 +1,100 @@
-from collections import OrderedDict
+from collections import deque 
 input_lines = open('./input.txt','r').readlines()
 
 class Wire:
     def __init__(self, wire_line):
-        self.last_point = (0,0)
-        self.points = OrderedDict() 
+        # key is the pont, value is a set of connected points 
+        self.points = {} 
+
+        # add initial point
+        self.points[(0,0)] = set() 
+        self.last_point = (0,0) 
+
         instructions = wire_line.strip().split(',')
         for instruction in instructions:
             direction = instruction[0]
             distance = int(instruction[1:])
             for _ in range(distance):
-                self.addPoint(direction)
+                self.add_point(direction)
 
-    def addPoint(self, direction):
-        x,y = self.last_point
+    def add_point(self, direction):
+        x,y = self.last_point 
+
         if direction == 'U':
-            self.last_point = (x, y+1)
+            point = (x, y+1)
         elif direction == 'D':
-            self.last_point = (x, y-1)
+            point = (x, y-1)
         elif direction == 'R':
-            self.last_point = (x+1, y)
+            point = (x+1, y)
         elif direction == 'L':
-            self.last_point = (x-1, y)
+            point = (x-1, y)
 
-        self.points[self.last_point] = 0
+        # if we are crossing an existing point add edge to last point  
+        if point in self.points:
+            self.points[point].add(self.last_point)
+        else:
+            # add new point with an edge to last point
+            self.points[point] = set([self.last_point])
 
-    def toSet(self):
+        # we want to connect point both ways, so add a backwards edge to new point
+        self.points[self.last_point].add(point)
+
+        self.last_point = point
+
+    def to_set(self):
         return set(self.points.keys())
 
-wires = map(Wire, input_lines)
-intersections = set.intersection(*map(lambda wire:  wire.toSet(), wires))
-min_distance = min(map(lambda p: abs(p[0]) + abs(p[1]), intersections))
+    def shortest_path_search(self, search_points):
+        found_distances = {}
+        seen = set()
 
-print(min_distance)
+        # start our queue at 0,0 
+        queue = deque()
+        queue.append(((0,0), 0))
+
+        while len(queue) > 0 and len(search_points) > len(found_distances):
+            # pull next item from the queue
+            (point, depth) = queue.popleft()
+            # out points connect both ways so we need to keep track of visited points
+            # to prevent going backwards and infinite loops 
+            seen.add(point)
+
+            # save depth if we are searching for this point
+            if point in search_points:
+                found_distances[point] = depth
+
+            # add all connected points to the queue
+            for edge_point in self.points[point]
+                if edge_point not in seen:
+                    queue.append((edge_point, depth + 1))
+        return found_distances;
+
+
+
+
+wire_a = Wire(input_lines[0])
+wire_b = Wire(input_lines[1])
+
+intersections = wire_a.to_set().intersection(wire_b.to_set())
+
+# remove start position
+intersections.remove((0,0))
+intersections = list(intersections)
+
+# part 1
+print('closest intersection:')
+print(min([abs(i[0])+abs(i[1]) for i in intersections]))
+
+# search for shortest paths to intersections 
+wire_a_distances = wire_a.shortest_path_search(intersections)
+wire_b_distances = wire_b.shortest_path_search(intersections)
+
+sums = [wire_a_distances[i] + wire_b_distances[i] for i in intersections]
+min_sum = min(sums)
+p = intersections[sums.index(min_sum)]
+
+print('shortest path:')
+print(p)
+print(min_sum)
+
+
